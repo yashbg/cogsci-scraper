f8ca73ad1a4318fd9dc457d2781d823668e19a6c
Yash Gupta <yashbg7@gmail.com>
2023-05-02 13:39:09 +0530

    Add IMPLICIT_WAIT_TIME and MAX_CHOICE

--- main.py
+++ main.py
@@ -5,11 +5,13 @@ from selenium.webdriver.common.by import By
 import random
 import time
 
+IMPLICIT_WAIT_TIME = 10
 URL_LOGIN = 'http://langcoglabcgsiitk.in/survey/login.php'
 URL_PROJECT = 'http://langcoglabcgsiitk.in/survey/survey.php?surveyid=18'
 NUM_ITER = 15000
 WAIT_TIME = 5
 DONT_KNOW_PROB = 0.1
+MAX_CHOICE = 7
 
 random.seed()
 chrome_options = Options()
@@ -22,7 +24,7 @@ email = parser.get('creds', 'email')
 password = parser.get('creds', 'password')
 
 driver = Chrome(options=chrome_options)
-driver.implicitly_wait(10)
+driver.implicitly_wait(IMPLICIT_WAIT_TIME)
 
 driver.get(URL_LOGIN)
 driver.find_element(By.NAME, 'login_username').send_keys(email) # email
@@ -42,7 +44,7 @@ for _ in range(NUM_ITER):
         if random.random() < DONT_KNOW_PROB:
             choice = 0
         else:
-            choice = random.randrange(1, 8)
+            choice = random.randrange(1, MAX_CHOICE + 1)
         choice_dict[question] = choice
 
     driver.find_element(By.CSS_SELECTOR, f'label[for="radio{choice}"]').click() # choice
